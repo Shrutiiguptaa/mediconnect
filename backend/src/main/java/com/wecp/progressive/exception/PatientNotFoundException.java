943150ee0fae2303dd42418b6c8765cec23a4244
username <wecp-dummy@gmail.com>
2025-09-24 13:12:59 +0000

    day 05 done

--- backend/src/main/java/com/wecp/progressive/exception/PatientNotFoundException.java
+++ backend/src/main/java/com/wecp/progressive/exception/PatientNotFoundException.java
@@ -1,4 +1,17 @@
 package com.wecp.progressive.exception;
 
+import java.sql.SQLException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class PatientNotFoundException {
+
+    @ExceptionHandler(SQLException.class)
+    public ResponseEntity<String> handleException(SQLException e) {
+        return new ResponseEntity<>("Patient not found", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
