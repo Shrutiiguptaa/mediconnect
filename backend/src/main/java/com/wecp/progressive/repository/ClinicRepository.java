98be4bc8d4a68d22b4c9938933b7e0e6cfd337a0
username <wecp-dummy@gmail.com>
2025-09-26 18:10:22 +0000

    wecp initial commit

--- backend/src/main/java/com/wecp/progressive/repository/ClinicRepository.java
+++ backend/src/main/java/com/wecp/progressive/repository/ClinicRepository.java
@@ -1,4 +1,22 @@
 package com.wecp.progressive.repository;
-
-public interface ClinicRepository {
+ 
+import java.util.List;
+ 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+ 
+import com.wecp.progressive.entity.Clinic;
+ 
+@Repository
+public interface ClinicRepository extends JpaRepository<Clinic, Integer> {
+   
+    Clinic findByClinicId(int clinicId);
+   
+    List<Clinic> findAllByLocation(String location);
+   
+    @Query("SELECT c FROM Clinic c WHERE c.doctorId = :doctorId")
+    List<Clinic> findAllByDoctorId(@Param("doctorId") int doctorId);
 }
+ 
